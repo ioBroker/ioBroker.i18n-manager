87bf4cc54c735cd3823f415f357ff0635b785801
Michele Granato <michele.granato985@gmail.com>
2023-11-29 08:39:10 +0100

    path stampato in fondo

--- src/folder/views/Folder/Folder.vue
+++ src/folder/views/Folder/Folder.vue
@@ -3,7 +3,11 @@
     <v-row>
       <v-col class="pa-0">
         <v-card tile>
-          <v-toolbar color="primary" dark class="flex-grow-0">
+          <v-toolbar
+            color="primary"
+            dark
+            class="flex-grow-0"
+          >
             <v-row>
               <v-col cols="2">
                 <v-select
@@ -35,7 +39,10 @@
       </v-col>
     </v-row>
     <v-row class="main ma-0 mt-2">
-      <v-col cols="4" class="left-side pa-0 pb-1 mr-4">
+      <v-col
+        cols="4"
+        class="left-side pa-0 pb-1 mr-4"
+      >
         <v-card class="left-card">
           <RecycleScroller
             :items="expandedTreeItems"
@@ -88,9 +95,24 @@
     </v-row>
     <v-row class="status-bar">
       <v-col class="pb-0 pt-2">
-        <v-card tile>
-          <div class="status-item" v-if="isSaving">
+        <v-card
+          tile
+          class="d-flex align-center"
+        >
+          <div
+            class="status-item animate"
+            v-if="isSaving"
+          >
             <v-icon>mdi-content-save-outline</v-icon>
+          </div>
+          <div
+            class="status-item"
+            v-if="!isSaving && selectedItem"
+          >
+            <template v-for="(item, index) in selectedItemPath(selectedItem)">
+              <span :key="item">{{ item }}</span>
+              <span :key="index"><v-icon v-if="index < selectedItemPath(selectedItem).length - 1">mdi-arrow-right</v-icon></span>
+            </template>
           </div>
         </v-card>
       </v-col>
@@ -120,221 +142,227 @@
 </template>
 
 <script lang="ts">
-  import { defineComponent, ref } from '@vue/composition-api';
+import { defineComponent, ref } from '@vue/composition-api';
 
-  import {
-    AddItemPayload,
-    ChangeFolderValuePayload,
-    ClipboardItemAction,
-    LanguageListItem,
-    PasteItemPayload,
-    TranslationError,
-    TranslationProgress,
-    TreeItem,
-    TreeMap,
-  } from '@/folder/types';
-  import { useNamespace } from '@/store/utils';
-  import { CustomSettings, LoadedPath } from '@common/types';
-  import useTree from './compositions/tree';
+import {
+  AddItemPayload,
+  ChangeFolderValuePayload,
+  ClipboardItemAction,
+  LanguageListItem,
+  PasteItemPayload,
+  TranslationError,
+  TranslationProgress,
+  TreeItem,
+  TreeMap,
+} from '@/folder/types';
+import { useNamespace } from '@/store/utils';
+import { CustomSettings, LoadedPath } from '@common/types';
+import useTree from './compositions/tree';
 
-  import Content from '../../components/Content.vue';
-  import Translate from '../../components/Translate.vue';
-  import TranslationProgressPanel from '../../components/TranslationProgressPanel.vue';
-  import Tree from '../../components/Tree.vue';
-  import ContextMenu from '../../components/ContextMenu.vue';
+import Content from '../../components/Content.vue';
+import Translate from '../../components/Translate.vue';
+import TranslationProgressPanel from '../../components/TranslationProgressPanel.vue';
+import Tree from '../../components/Tree.vue';
+import ContextMenu from '../../components/ContextMenu.vue';
 
-  export default defineComponent({
-    name: 'Folder',
-    components: { TranslationProgressPanel, Translate, Content, Tree, ContextMenu },
-    setup() {
-      const globalModule = useNamespace('global');
-      const showSettings = globalModule.useMutation('showSettings');
+export default defineComponent({
+  name: 'Folder',
+  components: { TranslationProgressPanel, Translate, Content, Tree, ContextMenu },
+  setup() {
+    const globalModule = useNamespace('global');
+    const showSettings = globalModule.useMutation('showSettings');
 
-      const settingsModule = useNamespace('settings');
-      const settings = settingsModule.useState<CustomSettings>('settings');
+    const settingsModule = useNamespace('settings');
+    const settings = settingsModule.useState<CustomSettings>('settings');
 
-      const folderModule = useNamespace('folder');
-      const tree = folderModule.useState<TreeMap>('tree');
-      const treeItems = folderModule.useGetter<TreeItem[]>('treeItems');
-      const folder = folderModule.useState<LoadedPath[]>('folder');
-      const originalFolder = folderModule.useState<LoadedPath[]>('originalFolder');
-      const selectedItem = folderModule.useState<TreeItem>('selectedItem');
-      const sendModifiedContent = folderModule.useAction('sendModifiedContent');
-      const setSelectedItem = folderModule.useMutation('setSelectedItem');
-      const updateValue = folderModule.useMutation('updateValue');
-      const updateTreeStatus = folderModule.useMutation('updateTreeStatus');
-      const deleteItem = folderModule.useMutation('deleteItem');
-      const renameItem = folderModule.useMutation('renameItem');
-      const addItem = folderModule.useMutation('addItem');
+    const folderModule = useNamespace('folder');
+    const tree = folderModule.useState<TreeMap>('tree');
+    const treeItems = folderModule.useGetter<TreeItem[]>('treeItems');
+    const folder = folderModule.useState<LoadedPath[]>('folder');
+    const originalFolder = folderModule.useState<LoadedPath[]>('originalFolder');
+    const selectedItem = folderModule.useState<TreeItem>('selectedItem');
+    const sendModifiedContent = folderModule.useAction('sendModifiedContent');
+    const setSelectedItem = folderModule.useMutation('setSelectedItem');
+    const updateValue = folderModule.useMutation('updateValue');
+    const updateTreeStatus = folderModule.useMutation('updateTreeStatus');
+    const deleteItem = folderModule.useMutation('deleteItem');
+    const renameItem = folderModule.useMutation('renameItem');
+    const addItem = folderModule.useMutation('addItem');
 
-      const languageList = folderModule.useState<LanguageListItem[]>('languageList');
-      const isTranslationEnabled = folderModule.useState<boolean>('isTranslationEnabled');
-      const isTranslating = folderModule.useState<boolean>('isTranslating');
-      const translationProgress = folderModule.useState<TranslationProgress>('translationProgress');
-      const translationErrors = folderModule.useState<TranslationError[]>('translationErrors');
-      const translate = folderModule.useAction('translate');
-      const cancelTranslate = folderModule.useAction('cancelTranslate');
-      const setIsTranslating = folderModule.useMutation('setIsTranslating');
+    const languageList = folderModule.useState<LanguageListItem[]>('languageList');
+    const isTranslationEnabled = folderModule.useState<boolean>('isTranslationEnabled');
+    const isTranslating = folderModule.useState<boolean>('isTranslating');
+    const translationProgress = folderModule.useState<TranslationProgress>('translationProgress');
+    const translationErrors = folderModule.useState<TranslationError[]>('translationErrors');
+    const translate = folderModule.useAction('translate');
+    const cancelTranslate = folderModule.useAction('cancelTranslate');
+    const setIsTranslating = folderModule.useMutation('setIsTranslating');
 
-      const isSaving = folderModule.useState<boolean>('isSaving');
+    const isSaving = folderModule.useState<boolean>('isSaving');
 
-      const setClipboard = folderModule.useMutation('setClipboard');
-      const pasteItem = folderModule.useMutation('pasteItem');
-      const clipboardItemId = folderModule.useState<string>('clipboardItemId');
-      const clipboardItemAction = folderModule.useState<ClipboardItemAction>('clipboardItemAction');
+    const setClipboard = folderModule.useMutation('setClipboard');
+    const pasteItem = folderModule.useMutation('pasteItem');
+    const clipboardItemId = folderModule.useState<string>('clipboardItemId');
+    const clipboardItemAction = folderModule.useState<ClipboardItemAction>('clipboardItemAction');
 
-      const treeComposition = useTree(tree, treeItems, folder);
+    const treeComposition = useTree(tree, treeItems, folder);
 
-      const contextMenuRef = ref<any>(null);
+    const contextMenuRef = ref<any>(null);
 
-      function selectItem(item: TreeItem) {
-        setSelectedItem(item);
-        if (item.type !== 'item') {
-          treeComposition.toggleTreeNode(item);
-        }
+    function selectItem(item: TreeItem) {
+      setSelectedItem(item);
+      if (item.type !== 'item') {
+        treeComposition.toggleTreeNode(item);
       }
+    }
 
-      function updateFolderValue(payload: ChangeFolderValuePayload) {
-        updateValue(payload);
-        updateTreeStatus();
-        sendModifiedContent();
-      }
+    function updateFolderValue(payload: ChangeFolderValuePayload) {
+      updateValue(payload);
+      updateTreeStatus();
+      sendModifiedContent();
+    }
 
-      function onAddItem(payload: AddItemPayload) {
-        addItem(payload);
-        treeComposition.expandTreeNode(payload.parent);
-      }
+    function onAddItem(payload: AddItemPayload) {
+      addItem(payload);
+      treeComposition.expandTreeNode(payload.parent);
+    }
 
-      function onPasteItem(payload: PasteItemPayload) {
-        pasteItem(payload);
-        treeComposition.expandTreeNode(payload.parent);
-      }
+    function onPasteItem(payload: PasteItemPayload) {
+      pasteItem(payload);
+      treeComposition.expandTreeNode(payload.parent);
+    }
 
-      function handleItemRightClick(event: MouseEvent, item: TreeItem) {
-        contextMenuRef.value?.handleRightClick(event, item);
-      }
+    function handleItemRightClick(event: MouseEvent, item: TreeItem) {
+      contextMenuRef.value?.handleRightClick(event, item);
+    }
 
-      return {
-        showSettings,
-        settings,
-        folder,
-        originalFolder,
-        selectedItem,
-        selectItem,
-        updateFolderValue,
+    function selectedItemPath(item: TreeItem) {
+      return item.path.slice(item.path.length - item.level, item.path.length);
+    }
 
-        tree,
-        treeItems,
+    return {
+      showSettings,
+      settings,
+      folder,
+      originalFolder,
+      selectedItem,
+      selectItem,
+      updateFolderValue,
 
-        ...treeComposition,
-        contextMenuRef,
-        handleItemRightClick,
-        onAddItem,
-        onPasteItem,
-        renameItem,
-        deleteItem,
-        setClipboard,
-        sendModifiedContent,
+      tree,
+      treeItems,
 
-        languageList,
-        isTranslationEnabled,
-        translate,
-        isTranslating,
-        translationProgress,
-        translationErrors,
-        setIsTranslating,
-        cancelTranslate,
+      ...treeComposition,
+      contextMenuRef,
+      handleItemRightClick,
+      onAddItem,
+      onPasteItem,
+      renameItem,
+      deleteItem,
+      setClipboard,
+      sendModifiedContent,
+      selectedItemPath,
 
-        isSaving,
+      languageList,
+      isTranslationEnabled,
+      translate,
+      isTranslating,
+      translationProgress,
+      translationErrors,
+      setIsTranslating,
+      cancelTranslate,
 
-        clipboardItemId,
-        clipboardItemAction,
-      };
-    },
-  });
+      isSaving,
+
+      clipboardItemId,
+      clipboardItemAction,
+    };
+  },
+});
 </script>
 
 <style scoped lang="scss">
-  .folder {
-    height: 100vh;
-  }
+.folder {
+  height: 100vh;
+}
 
-  .main {
-    flex: 1;
-    height: calc(100vh - 64px - 46px);
-  }
+.main {
+  flex: 1;
+  height: calc(100vh - 64px - 46px);
+}
 
-  .left-side {
+.left-side {
+  height: 100%;
+}
+
+.left-card {
+  border-top-left-radius: 0 !important;
+  border-top-right-radius: 0 !important;
+
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+
+.tree-container {
+  overflow: auto;
+  height: 100%;
+
+  .tree-scroller {
     height: 100%;
   }
+}
 
-  .left-card {
-    border-top-left-radius: 0 !important;
-    border-top-right-radius: 0 !important;
+.right-side {
+  padding-top: 0;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
 
+  .content-container {
     height: 100%;
-    display: flex;
-    flex-direction: column;
-  }
-
-  .tree-container {
     overflow: auto;
-    height: 100%;
 
-    .tree-scroller {
+    > div {
       height: 100%;
+      overflow-y: auto;
+      overflow-x: hidden;
     }
   }
+}
 
-  .right-side {
-    padding-top: 0;
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-
-    .content-container {
-      height: 100%;
-      overflow: auto;
-
-      > div {
-        height: 100%;
-        overflow-y: auto;
-        overflow-x: hidden;
-      }
-    }
+.status-bar {
+  .v-card {
+    height: 30px;
   }
 
-  .status-bar {
-    .v-card {
-      height: 30px;
-    }
-
-    .status-item {
-      padding: 0 8px;
-
+  .status-item {
+    padding: 0 8px;
+    &.animate {
       animation: status-animation 2s infinite;
     }
   }
+}
 
-  @keyframes status-animation {
-    0% {
-      opacity: 0;
-    }
-    50% {
-      opacity: 1;
-    }
-    100% {
-      opacity: 0;
-    }
+@keyframes status-animation {
+  0% {
+    opacity: 0;
   }
+  50% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+  }
+}
 </style>
 
 <style lang="scss">
-  .tree-container {
-    .tree-scroller {
-      > div {
-        overflow: visible;
-      }
+.tree-container {
+  .tree-scroller {
+    > div {
+      overflow: visible;
     }
   }
+}
 </style>
